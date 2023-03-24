60582b86fc7a55c88ad350a8a7481dd99b21e023
nadiamohamedtaha9 <nadiamohamedtaha9@gmail.com>
2023-03-24 07:44:11 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_line - function that draws a straight line in the terminal
+ * @n: intiger params
+ */
+void print_line(int n)
+{
+	int x;
+
+	if (n < 0)
+	{
+		_putchar('\n');
+	}
+	for (x = 0; x <= n; x++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+}
