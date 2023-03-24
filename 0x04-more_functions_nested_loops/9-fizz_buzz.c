fb1f5c5464f742650c9cf40494066b002fe6c219
nadiamohamedtaha9 <nadiamohamedtaha9@gmail.com>
2023-03-24 08:44:11 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * fizz_buzz - printing to 100
+ */
+void fizz_buzz(void)
+{
+	int x;
+
+	for (x = 0; x < 100; x++)
+	{
+		if (x % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (x % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (x % 3 == 0 && x % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+	}
+}
