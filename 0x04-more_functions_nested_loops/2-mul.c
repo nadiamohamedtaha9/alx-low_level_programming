ddd1881fd7cf1e16c45f191323735e34ccf5abad
nadiamohamedtaha9 <nadiamohamedtaha9@gmail.com>
2023-03-24 06:20:52 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multiplying two numbers
+ * @a: intiger param 1
+ * @b: intiger param 2
+ * Return: success and intiger
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
