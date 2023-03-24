a3e95df05f4a43c7a340e394a11911a36605bbe6
nadiamohamedtaha9 <nadiamohamedtaha9@gmail.com>
2023-03-24 08:16:03 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -15,13 +15,12 @@ void print_diagonal(int n)
 	{
 		for (x = 0; x < n ; x++)
 		{
-			for (y = 0; y <= x; y++)
+			for (y = 0; y < x; y++)
 			{
 				_putchar(' ');
 			}
 			_putchar(92);
 			_putchar('\n');
 		}
-		_putchar('\n');
 	}
-}
+
