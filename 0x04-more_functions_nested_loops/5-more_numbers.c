396df6ea9241ce29a8f4f90439981e364c3d83c4
nadiamohamedtaha9 <nadiamohamedtaha9@gmail.com>
2023-03-24 07:02:01 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,7 +10,14 @@ void more_numbers(void)
 	{
 		for (b = 0; b < 15; b++)
 		{
-			_putchar(b + '0');
+			if (b >= 10)
+			{
+				_putchar((b / 10) + '0');
+			}
+			else
+			{
+				_putchar((b % 10) + '0');
+			}
 		}
 		_putchar('\n');
 	}
