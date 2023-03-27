241d105bd449a1c38dd1b566c168dcf86fb7f88b
nadiamohamedtaha9 <nadiamohamedtaha9@gmail.com>
2023-03-27 14:22:35 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - function that prints a string
+ * @str: string parameter to be brint
+ * Return: Nothing
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str = 0);
+		str++;
+	}
+	_Putchar('\n');
+}
