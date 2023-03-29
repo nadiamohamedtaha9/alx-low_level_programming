578dddd764ce924ec6bf4d60ee13835b937005ee
nadiamohamedtaha9 <nadiamohamedtaha9@gmail.com>
2023-03-29 15:46:19 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -18,7 +18,7 @@ char *_strncat(char *dest, char *src, int n)
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[c + i] = src[i];
-		dest[c + i] = '\0';
 	}
+	dest[c + i] = '\0';
 	return (dest);
 }
